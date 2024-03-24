61f8105956ff850cf37f33e79b1677c5684a0f32
Royer Gaétan <royergaetan90@gmail.com>
2024-03-24 23:22:20 +0100

    first commit

---
+++ src/com/codeineflow/main/KeyHandler.java
@@ -0,0 +1,42 @@
+package com.codeineflow.main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+	public static boolean upPressed, downPressed, leftPressed, rightPressed, pausePressed;
+	
+	@Override
+	public void keyTyped(KeyEvent e) {}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+		int code = e.getKeyCode();
+		
+		if(code == KeyEvent.VK_Z) {
+			upPressed = true;
+		}
+		
+		if(code == KeyEvent.VK_Q) {
+			leftPressed = true;
+		}
+		
+		if(code == KeyEvent.VK_S) {
+			downPressed = true;
+		}
+		
+		if(code == KeyEvent.VK_D) {
+			rightPressed = true;
+		}
+		
+		if(code == KeyEvent.VK_SPACE) {
+			pausePressed = !pausePressed;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {}
+	
+}
---
+++ src/com/codeineflow/main/PlayManager.java
@@ -0,0 +1,156 @@
+package com.codeineflow.main;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.RenderingHints.Key;
+import java.util.ArrayList;
+import java.util.Random;
+
+import com.codeineflow.mino.Block;
+import com.codeineflow.mino.Mino;
+import com.codeineflow.mino.Mino_Bar;
+import com.codeineflow.mino.Mino_L1;
+import com.codeineflow.mino.Mino_L2;
+import com.codeineflow.mino.Mino_Square;
+import com.codeineflow.mino.Mino_T;
+import com.codeineflow.mino.Mino_Z1;
+import com.codeineflow.mino.Mino_Z2;
+
+public class PlayManager {
+
+	// Main play area
+	final int WIDTH = 360; // 30 x 12 = 360 width
+	final int HEIGHT = 600; // 30 x 20 = 600 height
+	public static int left_x;
+	public static int right_x;
+	public static int top_y;
+	public static int bottom_y;
+
+	// Mino
+	Mino currentMino;
+	final int MINO_START_X;
+	final int MINO_START_Y;
+	Mino nextMino;
+	final int NEXTMINO_X;
+	final int NEXTMINO_Y;
+	public static ArrayList<Block> staticBlocks = new ArrayList<>();
+
+	// Others
+	public static int dropInterval = 60; // mino drops in every 60 frames (1 sec)
+
+	public PlayManager() {
+
+		// Main play area frame
+		left_x = (GamePanel.WIDTH / 2) - (WIDTH / 2);
+		right_x = left_x + WIDTH;
+		top_y = 50;
+		bottom_y = top_y + HEIGHT;
+
+		MINO_START_X = left_x + (WIDTH / 2) - Block.SIZE;
+		MINO_START_Y = top_y + Block.SIZE;
+
+		NEXTMINO_X = right_x + 175;
+		NEXTMINO_Y = top_y + 500;
+
+		// Set the starting mino
+		currentMino = pickMino();
+		currentMino.setXY(MINO_START_X, MINO_START_Y);
+		nextMino = pickMino();
+		nextMino.setXY(NEXTMINO_X, NEXTMINO_Y);
+	}
+
+	public Mino pickMino() {
+		Mino mino = null;
+		int i = new Random().nextInt(7);
+
+		switch (i) {
+		case 0:
+			mino = new Mino_L1();
+			break;
+		case 1:
+			mino = new Mino_L2();
+			break;
+		case 2:
+			mino = new Mino_Square();
+			break;
+		case 3:
+			mino = new Mino_Bar();
+			break;
+		case 4:
+			mino = new Mino_T();
+			break;
+		case 5:
+			mino = new Mino_Z1();
+			break;
+		case 6:
+			mino = new Mino_Z2();
+			break;
+		}
+
+		return mino;
+	}
+
+	public void update() {
+		// Check if the currentMino is active
+		if (currentMino.active == false) {
+
+			// if the mino is active, put it into the staticBlocks
+			staticBlocks.add(currentMino.b[0]);
+			staticBlocks.add(currentMino.b[1]);
+			staticBlocks.add(currentMino.b[2]);
+			staticBlocks.add(currentMino.b[3]);
+
+			currentMino.deactivating = false;
+
+			// replace the currentMino with the nextMino
+			currentMino = nextMino;
+			currentMino.setXY(MINO_START_X, MINO_START_Y);
+			nextMino = pickMino();
+			nextMino.setXY(NEXTMINO_X, NEXTMINO_Y);
+		} else {
+			currentMino.update();
+		}
+	}
+
+	public void draw(Graphics2D g2) {
+
+		// Draw play area frame
+		g2.setColor(Color.white);
+		g2.setStroke(new BasicStroke(4f));
+		g2.drawRect(left_x - 4, top_y - 4, WIDTH + 8, HEIGHT + 8);
+
+		// Draw next mino frame
+		int x = right_x + 100;
+		int y = bottom_y - 200;
+		g2.drawRect(x, y, 200, 200);
+		g2.setFont(new Font("Arial", Font.PLAIN, 30));
+		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g2.drawString("NEXT", x + 60, y + 60);
+
+		// Draw the current Mino
+		if (currentMino != null) {
+			currentMino.draw(g2);
+		}
+
+		// Draw the next Mino
+		nextMino.draw(g2);
+
+		// Draw static blocks
+		for (int i = 0; i < staticBlocks.size(); i++) {
+			staticBlocks.get(i).draw(g2);
+		}
+
+		// Draw pause
+		g2.setColor(Color.yellow);
+		g2.setFont(g2.getFont().deriveFont(50f));
+
+		if (KeyHandler.pausePressed) {
+			x = left_x + 80;
+			y = top_y + 320;
+			g2.drawString("PAUSED", x, y);
+		}
+	}
+}
